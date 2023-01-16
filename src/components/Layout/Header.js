2b3adabc7e469e44da2a74f1ffbc47530505e1b9
Katarzyna Śliwka <katarzynasliwka@Katarzynas-MacBook-Pro.local>
2023-01-16 13:08:09 +0100

    dinal satge

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,12 +3,12 @@ import { Fragment } from "react";
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1 className={classes.h1}>Foody</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart}/>
       </header>
     </Fragment>
   );
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,21 @@
-import classes from './HeaderCartButton.module.css'
+import { useContext } from "react";
+
+import classes from "./HeaderCartButton.module.css";
 import cartIcon from "../../assets/shopping_cart.svg";
+import CartContext from "../../store/cart-context";
 
-const HeaderCartButton = () => {
+const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
   return (
-    <button className={classes.button}>
-      
-      <img src={cartIcon} />
-      <a className={classes.badge}><span className={classes.text}>3</span></a>
+    <button className={classes.button} onClick={props.onClick}>
+      <img src={cartIcon} alt="Icon" />
+      <a className={classes.badge}>
+        <span className={classes.text}>{numberOfCartItems}</span>
+      </a>
     </button>
-
   );
 };
 
